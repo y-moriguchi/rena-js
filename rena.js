344193d9ba457a6624e66700008d189b3d16b2da
y-moriguchi <yuichiro-moriguchi@nifty.com>
2018-02-24 11:13:27 +0900

    supports lookahead and repetitions

--- rena.js
+++ rena.js
@@ -58,19 +58,33 @@
 		actions[actionId] = action;
 		this.action = actionId++;
 	}
-	function StarMaybe(captureName) {
-		this.captureName = captureName;
+	function Repeat(action, init, addr, minimum) {
+		this.action = action;
+		this.init = init;
+		this.addr = addr;
+		this.minimum = minimum;
 	}
-	function Alt(captureName, alternates) {
-		this.captureName = captureName;
+	function Alt(action, alternates) {
+		this.action = action;
 		this.alternates = alternates;
 	}
-	function GoTo(captureName) {
-		this.captureName = captureName;
+	function GoTo(action, addr, maximum) {
+		this.action = action;
+		this.addr = addr;
+		this.maximum = maximum;
 	}
 	function Then(pattern, action) {
 		this.pattern = wrap(pattern);
 		this.action = action;
+	}
+	function Lookahead(pattern, positive) {
+		this.pattern = wrap(pattern);
+		this.positive = positive;
+	}
+	function Cond(cond) {
+		this.cond = cond;
+	}
+	function PassAll() {
 	}
 	function Rena(attribute) {
 		if(!(this instanceof Rena)) {
@@ -79,77 +93,99 @@
 		this._patterns = [];
 		this._attribute = attribute;
 	}
-	function testRe(str, startIndex, rena, startpc, captures, attribute) {
+	function testRe(str, startIndex, rena, startpc, captures, attribute, countRepetation) {
 		var pc = startpc,
 			index = startIndex,
 			attr = attribute,
+			passAction = false,
+			count = countRepetation ? countRepetation : 0,
 			match,
 			inst,
-			count = 0,
 			beforeLength,
 			i;
 		function executeAction(attr, action, match) {
-			if(action) {
+			if(passAction) {
+				return void 0;
+			} else if(action) {
 				return nvf(attr, action(match.match, match.attribute, attr));
 			} else {
 				return nvf(attr, match.attribute);
 			}
 		}
+		function executeRepeatAction(attr, inst) {
+			if(inst.action) {
+				for(i = 0; i < captures[inst.action].length; i++) {
+					attr = executeAction(attr, actions[inst.action], captures[inst.action][i]);
+				}
+			}
+			return attr;
+		}
 		captures = captures || {};
 		outer: while(pc < rena._patterns.length) {
 			inst = rena._patterns[pc];
-			if(inst instanceof NewAction) {
-				if(inst.action && !captures[inst.action]) {
-					captures[inst.action] = [];
-				}
-				pc++;
-			} else if(inst instanceof StarMaybe) {
-				if(!!(match = testRe(str, index, rena, pc + 1, captures, attr))) {
-					attr = nvf(attr, match.attribute);
-					if(inst.captureName) {
-						if(count === 0) {
-							for(i = 0; i < captures[inst.captureName].length; i++) {
-								attr = executeAction(attr, actions[inst.captureName], match);
-							}
-						}
-					}
-					return {
-						match: str.substring(startIndex, match.lastIndex),
-						lastIndex: match.lastIndex,
-						attribute: attr
-					};
-				} else {
-					if(inst.captureName) {
-						captures[inst.captureName].pop();
-					}
-					pc = pc + 3;
-				}
-			} else if(inst instanceof Alt) {
-				for(i = 0; i < inst.alternates.length; i++) {
-					if(!!(match = inst.alternates[i](str, index, attr))) {
-						if(inst.captureName) {
-							captures[inst.captureName] = matchNext.match;
-						}
-						index = match.lastIndex;
-						attr = nvf(attr, match.attribute);
-						pc++;
-						continue outer;
-					}
-				}
-				return null;
-			} else if(inst instanceof GoTo) {
-				if(inst.captureName) {
-					captures[inst.captureName].push(match);
-				}
-				count++;
-				pc = match.match === "" ? pc + 1 : pc - 2;
-			} else if(inst instanceof Then) {
+			if(inst instanceof Then) {
 				if(!!(match = inst.pattern(str, index, attr))) {
 					index = match.lastIndex;
 					attr = executeAction(attr, inst.action, match);
 				} else {
 					return null;
 				}
+				pc++;
+			} else if(inst instanceof NewAction) {
+				if(inst.action && !captures[inst.action]) {
+					captures[inst.action] = [];
+				}
+				pc++;
+			} else if(inst instanceof Repeat) {
+				if(!!(match = testRe(str, index, rena, pc + 1, captures, attr, count + 1))) {
+					attr = passAction ? void 0 : nvf(attr, match.attribute);
+					if(count === 0) {
+						attr = nvf(attr, inst.init);
+						attr = executeRepeatAction(attr, inst);
+					}
+					return {
+						match: str.substring(startIndex, match.lastIndex),
+						lastIndex: match.lastIndex,
+						attribute: attr
+					};
+				} else if(count >= inst.minimum) {
+					if(inst.captureName && captures[inst.action].length > 0) {
+						captures[inst.action].pop();
+					}
+					pc = pc + inst.addr;
+				} else {
+					return null;
+				}
+			} else if(inst instanceof Alt) {
+				for(i = 0; i < inst.alternates.length; i++) {
+					if(!!(match = inst.alternates[i](str, index, attr))) {
+						if(inst.action) {
+							captures[inst.action] = matchNext.match;
+						}
+						index = match.lastIndex;
+						attr = passAction ? void 0 : nvf(attr, match.attribute);
+						pc++;
+						continue outer;
+					}
+				}
+				return null;
+			} else if(inst instanceof GoTo) {
+				if(inst.action) {
+					captures[inst.action].push(match);
+				}
+				pc = match.match === "" || (inst.maximum >= 0 && count >= inst.maximum) ? pc + 1 : pc + inst.addr;
+			} else if(inst instanceof Lookahead) {
+				if(!inst.pattern(str, index, attr) === inst.positive) {
+					return null;
+				}
+				pc++;
+			} else if(inst instanceof Cond) {
+				if(!inst.cond(attr)) {
+					return null;
+				}
+				pc++;
+			} else if(inst instanceof PassAll) {
+				passAction = true;
 				pc++;
 			}
 		}
@@ -163,6 +199,9 @@
 		then: function(pattern, action) {
 			this._patterns.push(new Then(pattern, action));
 			return this;
+		},
+		thenPass: function(pattern) {
+			return this.then(pattern, Rena.pass);
 		},
 		or: function() {
 			var alts = [],
@@ -186,12 +225,44 @@
 			});
 			return this;
 		},
-		zeroOrMore: function(pattern, action) {
+		maybe: function(pattern, action) {
 			var action = new NewAction(action);
 			this._patterns.push(action);
-			this._patterns.push(new StarMaybe(action.action));
+			this._patterns.push(new Repeat(action.action, void 0, 2, 0));
 			this.then(wrap(pattern));
-			this._patterns.push(new GoTo(action.action));
+			return this;
+		},
+		times: function(countmin, countmax, pattern, action, init) {
+			var action = new NewAction(action);
+			this._patterns.push(action);
+			this._patterns.push(new Repeat(action.action, init, 3, countmin));
+			this.then(wrap(pattern));
+			this._patterns.push(new GoTo(action.action, -2, countmax));
+			return this;
+		},
+		atLeast: function(count, pattern, action, init) {
+			return this.times(count, -1, pattern, action, init);
+		},
+		atMost: function(count, pattern, action, init) {
+			return this.times(0, count, pattern, action, init);
+		},
+		zeroOrMore: function(pattern, action, init) {
+			return this.atLeast(0, pattern, action, init);
+		},
+		lookahead: function(pattern, positive) {
+			var pos = positive === void 0 ? true : positive;
+			this._patterns.push(new Lookahead(wrap(pattern), pos));
+			return this;
+		},
+		lookaheadNot: function(pattern) {
+			return this.lookahead(pattern, false);
+		},
+		cond: function(cond) {
+			this._patterns.push(new Cond(cond));
+			return this;
+		},
+		passAll: function() {
+			this._patterns.push(new PassAll());
 			return this;
 		},
 		test: function(str, index, attribute) {
@@ -202,6 +273,7 @@
 			return testRe(str, ind, this, 0, caps, attr);
 		}
 	};
+	Rena.pass = function() {};
 	Rena.delay = function(thunk) {
 		var memo = null;
 		return function(str, index) {
